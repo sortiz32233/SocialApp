bff3fbb9522a2dc0843626026514da205643e287
Steven Ortiz <68450906+sortiz32233@users.noreply.github.com>
2020-11-15 21:15:50 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/dipansh/socialapp/MainActivity.java
+++ app/src/main/java/com/example/dipansh/socialapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.dipansh.socialapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
